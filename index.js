ffce1432f4fabc83346bdbb2f31cc9e367c1b8fc
Lucas Kempe <lucas.kempe@icloud.com>
2020-11-12 14:17:16 -0300

    New commands

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ bot.on('message', (message) => {
         message.reply('Hola pibe, te estas portando bien?');
     }
     if(parts[0] === '!rango') {
-        message.reply('Tu nivel es: ' + userStats.level + 'te faltan: ' + userStats.xpToNextLvl + 'para el suguiente nivel.');
+        message.reply('Tu nivel es ' + userStats.level + ' te faltan ' + userStats.xpToNextLvl + ' para el suguiente nivel.');
     }
 });
 
